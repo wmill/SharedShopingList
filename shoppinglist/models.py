9e696b83f6646da8c4d11011c3ec39fe3385abe9
Walter Miller <walter@waltermiller.ca>
2014-12-08 02:08:26 -0500

    More work, fuck git commits for now

--- shoppinglist/models.py
+++ shoppinglist/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib import admin
+
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class ShoppingItem(models.Model):
-  name = models.CharField(max_length=200)
-  
+  name = models.CharField(max_length=400)
+  user = models.ForeignKey(User)
+
+  def __str__(self):
+    return self.name
+
+
+
+admin.site.register(ShoppingItem)
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from shoppinglist.models import ShoppingItem
 # Create your views here.
+
+#@login_required
+def index(request):
+  if request.method == "POST":
+    #creating or updating an item
+    
+    user = request.user
+    item_name = request.POST['item_name']
+    ShoppingItem.objects.create(name=item_name, user=user)
+
+
+  return defaultIndex(request)
+
+
+def defaultIndex(request):
+  #render list of items
+  #print dir(request.user)
+  context = {'shopping_items': ShoppingItem.objects.all()}
+  return render(request, 'index.html', context)
+
+def create(request):
+  if request.method == "POST":
+    
+    user = request.user
+    item_name = request.POST['item_name']
+    ShoppingItem.objects.create(name=item_name, user=user)
+
+
+def update(request):
+  pass
+
+def delete(request):
+  #we'll just use 
+  if request.method == "POST":
+    pass
