7a4ae42e30485994aee28070563f8db59966459d
Walter Miller <walter@waltermiller.ca>
2014-12-08 02:55:29 -0500

    updates

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -4,4 +4,9 @@ from shoppinglist import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^create$', views.create, name='create'),
+    url(r'^update$', views.update, name='update'),
+    url(r'^delete$', views.delete, name='delete'),
+    url(r'^logout$', views.logout_view, name='logout'),
+
 )
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,25 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from shoppinglist.models import ShoppingItem
+from django.contrib.auth import logout
 # Create your views here.
 
 #@login_required
 def index(request):
-  if request.method == "POST":
-    #creating or updating an item
-    
-    user = request.user
-    item_name = request.POST['item_name']
-    ShoppingItem.objects.create(name=item_name, user=user)
-
-
-  return defaultIndex(request)
-
-
-def defaultIndex(request):
   #render list of items
   #print dir(request.user)
   context = {'shopping_items': ShoppingItem.objects.all()}
   return render(request, 'index.html', context)
+
+
 
 def create(request):
   if request.method == "POST":
@@ -28,11 +19,36 @@ def create(request):
     item_name = request.POST['item_name']
     ShoppingItem.objects.create(name=item_name, user=user)
 
+  return redirect('index')
+
 
 def update(request):
-  pass
+  if request.method == "POST":
+    item_name = request.POST['item_name']
+    item_id = request.POST['item_id']
+    user = request.user
+
+    item = get_object_or_404(ShoppingItem, pk=item_id)
+
+    item.name = item_name
+    item.user = user
+
+    item.save()
+
+  return redirect('index')
+
+
 
 def delete(request):
   #we'll just use 
   if request.method == "POST":
-    pass
+    item_id = request.POST['item_id']
+    item = get_object_or_404(ShoppingItem, pk=item_id)
+    item.delete()
+
+  return redirect('index')
+
+
+def logout_view(request):
+  logout(request)
+  return redirect('index')
