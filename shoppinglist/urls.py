97ab0c53cfa3e21f0f2e63d8a6c52a062d8681a9
Walter Miller <walter@waltermiller.ca>
2014-12-08 04:45:09 -0500

    latest working code

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^create$', views.create, name='create'),
     url(r'^update$', views.update, name='update'),
-    url(r'^delete$', views.delete, name='delete'),
     url(r'^logout$', views.logout_view, name='logout'),
+    url(r'^accounts/login/?$', views.login_view, name='login'),
 
 )
--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from shoppinglist.models import ShoppingItem
-from django.contrib.auth import logout
-from django.http import HttpResponse
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
 
-
-#@login_required
+@login_required
 def index(request):
   #render list of items
   #print dir(request.user)
@@ -12,7 +11,7 @@ def index(request):
   return render(request, 'index.html', context)
 
 
-
+@login_required
 def create(request):
   if request.method == "POST":
     
@@ -22,7 +21,7 @@ def create(request):
 
   return redirect('index')
 
-
+@login_required
 def update(request):
   #this method also deletes
   if request.method == "POST":
@@ -52,17 +51,24 @@ def update(request):
   return redirect('index')
 
 
-
-def delete(request):
-  #we'll just use 
-  if request.method == "POST":
-    item_id = request.POST['item_id']
-    item = get_object_or_404(ShoppingItem, pk=item_id)
-    item.delete()
-
-  return redirect('index')
-
-
 def logout_view(request):
   logout(request)
-  return redirect('login_view')
+  return redirect('login')
+
+
+def login_view(request):
+  context = {}
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('index')
+        else:
+            context['flash_error'] = "Warning: Your account has been dissabled."
+    else:
+        context['flash_error'] = "Invalid username or password."
+
+  return render(request, 'login.html', context)
